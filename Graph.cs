9711a03608f2c5993856ebde7205093de0ac9021
mmf.rudakov <mmf.rudakov@MMF407-10>
2024-01-17 11:40:36 +0300

    add point

---
+++ Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Graph
+    {
+
+    }
+    public class Point
+    {
+        public string Status;
+        public bool Visit;
+        public Point()
+        {
+            Visit = false;
+        }
+        public Point(string status)
+            : base()
+        {
+            Status = status;
+        }
+    }
+}
